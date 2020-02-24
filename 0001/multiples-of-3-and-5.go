5c3afdff736a888ea053e7902390befd5b7b3c68
Piers Powlesland <pierspowlesland@gmail.com>
2020-02-24 14:47:59 +0000

    I think this approach is more performant
    It avoids use of divide, it could be more neatly expressed using a slice
    to hold the 2 counters.

--- 0001/multiples-of-3-and-5.go
+++ 0001/multiples-of-3-and-5.go
@@ -1,10 +1,35 @@
 package main
 
 func main() {
+	threes := 0
+	fives := 0
 	sum := 0
-	for i := 0; i<1000; i++ {
-		if i % 5 ==0 || i % 3 == 0 {
-			sum += i
+	complete := 0
+	for {
+		if threes <= fives && threes < 1000{
+			threes += 3
+			if threes >= 1000 {
+				complete ++
+				if complete == 2 {
+					break
+				}
+				continue
+			}
+			if threes != fives{
+				sum += threes
+			}
+		}else if fives < 1000{
+			fives += 5
+			if fives >= 1000 {
+				complete ++
+				if complete == 2 {
+					break
+				}
+				continue
+			}
+			if threes != fives{
+				sum += fives
+			}
 		}
 	}
 	println(sum)
